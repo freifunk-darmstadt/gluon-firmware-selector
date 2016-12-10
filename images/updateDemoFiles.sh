b0a3a851681f62b8c437f1b27ac5ba6d896d561e
Moritz Warning <moritzwarning@web.de>
2016-12-10 19:32:47 +0100

    remove some demo files

--- images/updateDemoFiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-curl -L https://firmware.darmstadt.freifunk.net/images/stable/factory > stable/factory/index.html
-curl -L https://firmware.darmstadt.freifunk.net/images/stable/sysupgrade > stable/sysupgrade/index.html
-curl -L https://firmware.darmstadt.freifunk.net/images/beta/factory > beta/factory/index.html
-curl -L https://firmware.darmstadt.freifunk.net/images/beta/sysupgrade > beta/sysupgrade/index.html
-curl -L https://firmware.darmstadt.freifunk.net/images/experimental/factory > experimental/factory/index.html
-curl -L https://firmware.darmstadt.freifunk.net/images/experimental/sysupgrade > experimental/sysupgrade/index.html
