2bfb456a83c73779cda790e6225718c1175d47e5
Moritz Warning <moritzwarning@web.de>
2016-12-11 02:40:27 +0100

    add explicit revision, might be slightly less confusing

--- devices.js
+++ devices.js
@@ -119,7 +119,7 @@ var vendormodels = {
     "TL-WR842N-ND": "tl-wr842n",
     "TL-WR843N-ND": "tl-wr843nd",
     "TL-WR940N-ND": {"tl-wr940n": "", "tl-wr941nd-v3": "v6"},
-    "TL-WR941N-ND": {"tl-wr941nd" : "", "tl-wr941nd-v3": ""},
+    "TL-WR941N-ND": {"tl-wr941nd" : "", "tl-wr941nd-v3": "v3"},
   },
 
   "Ubiquiti": {
