freifunk-darmstadt/gluon-firmware-selector

commit 59bad15a24afc8c59f46b71986d0a67e19d4b2d2

add 8devices Jalapeno Board

devices.js
@@ -1,6 +1,7 @@
 var devices_recommended = {
   "8devices": {
     "Carambola Board 2": "8devices-carambola2-board"
+    "Jalapeno Board": "8devices-jalapeno"
   },
 
   "Allnet": {

app.js
@@ -119,7 +119,7 @@ var firmwarewizard = function() {
     return index === self.indexOf(e);
   });
 
-  var reFileExtension = new RegExp(/.(bin|img.gz|img|tar)/);
+  var reFileExtension = new RegExp(/.(bin|img.gz|img|tar|ubi)/);
   var reRemoveDashes = new RegExp(/-/g);
   var reSearchable = new RegExp('[-/ '+NON_BREAKING_SPACE+']', 'g');
   var reRemoveSpaces = new RegExp(/ /g);

commit 768daa12cb7472a22e2429210cb0be3f1058c170

fix missing comma in devices.js
bug introduced in 59bad15a24afc8c59f46b71986d0a67e19d4b2d2

devices.js
@@ -1,6 +1,6 @@
 var devices_recommended = {
   "8devices": {
-    "Carambola Board 2": "8devices-carambola2-board"
+    "Carambola Board 2": "8devices-carambola2-board",
     "Jalapeno Board": "8devices-jalapeno"
   },
 