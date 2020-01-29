7fa7978bd1e2c0aa46cec49bfc64851d7f5bec39
Julian Labus <julian@labus-online.de>
2020-01-29 10:47:02 +0100

    devices: add Netgear EX6150 v1

--- devices.js
+++ devices.js
@@ -105,7 +105,7 @@ var devices_recommended = {
 
   "Netgear": {
     "EX6100": {"netgear-ex6100v2": "v2"},
-    "EX6150": {"netgear-ex6150v2": "v2"},
+    "EX6150": {"netgear-ex6150": "v1", "netgear-ex6150v2": "v2"},
     "R6120": "netgear-r6120",
     "R6220": "netgear-r6220",
     "R7800": "netgear-nighthawk-x4s-r7800",
