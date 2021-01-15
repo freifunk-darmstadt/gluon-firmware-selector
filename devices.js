dfeee2b59d01a4f9ad771f1041aae1ece80fbbc7
Julian Labus <julian@labus-online.de>
2021-01-15 09:30:18 +0100

    devices: add GL.iNet GL-USB150

--- devices.js
+++ devices.js
@@ -88,6 +88,7 @@ var devices_recommended = {
     "GL-MT300a": "gl-mt300a",
     "GL-MT300n": {"gl-mt300n": "v1", "gl-mt300n-v2": "v2"},
     "GL-MT750": "gl-mt750",
+    "GL-USB150": "gl.inet-gl-usb150",
     "VIXMINI": "gl.inet-vixmini"
   },
 
